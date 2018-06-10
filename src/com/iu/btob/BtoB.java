0eca3d05fbfacb8eae13110dabbf5bfd65b11a66
bomblike611 <33415746+bomblike611@users.noreply.github.com>
2018-06-10 13:11:57 +0900

    w
    w

---
+++ src/com/iu/btob/BtoB.java
@@ -0,0 +1,9 @@
+package com.iu.btob;
+
+public class BtoB {
+	//비투비 클래스를 작성 멤버변수로는 money와 name, 배열타입 mem
+	//멤버 메서드는 getSign을 가지며, 매개변수로 문자형을 받아 그대로 문자를 출력함
+
+	//메인함수에서 money의 값을 1000으로 초기화. name을 "성재"로 초기화 하시오.
+	//getSign함수를 호출하여 매개변수로 "창섭"을 입력
+}
